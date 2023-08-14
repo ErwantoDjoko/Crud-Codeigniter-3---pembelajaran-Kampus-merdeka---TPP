3262f9200d233f63e81162bc31cfe33a0e5483cd
Erwantojoko <joescore008@gmail.com>
2023-08-14 23:18:19 +0700

    update

--- application/views/main.php
+++ application/views/main.php
@@ -13,6 +13,22 @@
 
 <body>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="<?= base_url('../assets/img/kampus_mdk.png')?>" width="100px" style="position:relative;padding-right:5px;" alt=""><img src="<?= base_url('../assets/img/tpp.jpg')?>" width="100px" style="position:relative" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="<?= base_url('mahasiswa') ?>">Mahasiswa</a>
+        <a class="nav-link"  href="<?= base_url('mata_kuliah') ?>">Mata Kuliah</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
 <?php include_once($pages.'.php'); ?>
 
 </body>
---
+++ application/views/notfound.php
@@ -0,0 +1,2 @@
+
+<center><h5>Halaman ini akan menjadi pelatihan</h5></center>
